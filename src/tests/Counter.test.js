9ed2a8c0302de0d61431423751e867adcd50315d
macl <80979544+nmacl@users.noreply.github.com>
2024-04-09 12:56:48 -0400

    working tests

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,38 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Counter from '../components/Counter'; 
 
-beforeEach(() => {
-  // Render the Counter component here
-})
+describe('Counter component tests', () => {
+  beforeEach(() => {
+    render(<Counter />);
+  });
 
-test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
-});
+  test('renders counter component with initial count of 0', () => {
+    const counterElement = screen.getByTestId('count');
+    expect(counterElement).toHaveTextContent('0');
+  });
 
-test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
-});
+  test('increment button increases count by 1', async () => {
+    const incrementButton = screen.getByRole('button', { name: '+' });
+    await userEvent.click(incrementButton);
+    const counterElement = screen.getByTestId('count');
+    expect(counterElement).toHaveTextContent('1');
+  });
 
-test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
-});
+  test('decrement button decreases count by 1', async () => {
+    const decrementButton = screen.getByRole('button', { name: '-' });
+    await userEvent.click(decrementButton);
+    const counterElement = screen.getByTestId('count');
+    expect(counterElement).toHaveTextContent('-1');
+  });
 
-test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  test('clicking increment then decrement returns count to initial value', async () => {
+    const incrementButton = screen.getByRole('button', { name: '+' });
+    const decrementButton = screen.getByRole('button', { name: '-' });
+    await userEvent.click(incrementButton);
+    await userEvent.click(decrementButton);
+    const counterElement = screen.getByTestId('count');
+    expect(counterElement).toHaveTextContent('0');
+  });
 });
